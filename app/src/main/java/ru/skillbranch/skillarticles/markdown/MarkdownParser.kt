b7b943f2c78aab79527bb3beedbf57b0f35604e0
makweb <makweb@yandex.ru>
2020-02-17 19:25:30 +0400

    hometask_5

--- app/src/main/java/ru/skillbranch/skillarticles/markdown/MarkdownParser.kt
+++ app/src/main/java/ru/skillbranch/skillarticles/markdown/MarkdownParser.kt
@@ -17,12 +17,10 @@ object MarkdownParser {
     private const val RULE_GROUP = "(^[-_*]{3}$)"
     private const val INLINE_GROUP = "((?<!`)`[^`\\s].*?[^`\\s]?`(?!`))"
     private const val LINK_GROUP = "(\\[[^\\[\\]]*?]\\(.+?\\)|^\\[*?]\\(.*?\\))"
-    private const val BLOCK_CODE_GROUP = "(^```[\\s\\S]+?```$)" //group 10
-    private const val ORDER_LIST_GROUP = "(^\\d{1,2}\\.\\s.+?$)" //group 11
 
     //result regex
     private const val MARKDOWN_GROUPS = "$UNORDERED_LIST_ITEM_GROUP|$HEADER_GROUP|$QUOTE_GROUP" +
-            "|$ITALIC_GROUP|$BOLD_GROUP|$STRIKE_GROUP|$RULE_GROUP|$INLINE_GROUP|$LINK_GROUP|$BLOCK_CODE_GROUP|$ORDER_LIST_GROUP"
+            "|$ITALIC_GROUP|$BOLD_GROUP|$STRIKE_GROUP|$RULE_GROUP|$INLINE_GROUP|$LINK_GROUP"
 
     private val elementsPattern by lazy { Pattern.compile(MARKDOWN_GROUPS, Pattern.MULTILINE) }
 
@@ -39,132 +37,8 @@ object MarkdownParser {
      * clear markdown text to string without markdown characters
      */
     fun clear(string: String?): String? {
-        string ?: return null
-        var clearString = ""
-        val matcher = elementsPattern.matcher(string)
-        var lastStartIndex = 0
-
-        loop@ while (matcher.find(lastStartIndex)) {
-            val startIndex = matcher.start()
-            val endIndex = matcher.end()
-            // we found a mark
-            if (lastStartIndex < startIndex) {
-                // check what was before the mark
-                clearString += string.substring(lastStartIndex, startIndex)
-            }
-            // check what kind of mark this was
-            var text: String
-            val groups = 1..11
-            var group = -1
-            for (gr in groups) {
-                if (matcher.group(gr) != null) {
-                    group = gr
-                    break
-                }
-            }
-
-            when (group) {
-                //NOT FOUND -> BREAK
-                -1 -> break@loop
-                //1 -> LIST GROUP
-                1 -> {
-                    text = string.subSequence(startIndex.plus(2), endIndex).toString()
-                    val subs = clear(text)
-                    clearString += if (subs.isNullOrEmpty()) text else subs
-
-                    lastStartIndex = endIndex
-                }
-
-                //2 -> HEADER GROUP
-                2 -> {
-                    val reg = "^#{1,6}".toRegex().find(string.substring(startIndex, endIndex))
-                    val l = reg!!.value
-                    val level = reg!!.value.length
-
-                    clearString += string.substring(startIndex + level.inc(), endIndex)
-                    lastStartIndex = endIndex
-                }
-
-                //3 -> QUOTE_GROUP
-                3 -> {
-                    text = string.substring(startIndex.plus(2), endIndex)
-                    val subs = clear(text)
-                    clearString += if (subs.isNullOrEmpty()) text else subs
-                    lastStartIndex = endIndex
-                }
-
-                //4 -> ITALIC_GROUP
-                4 -> {
-                    text = string.substring(startIndex.inc(), endIndex.dec())
-                    val subs = clear(text)
-                    clearString += if (subs.isNullOrEmpty()) text else subs
-                    lastStartIndex = endIndex
-                }
-
-                //5 -> BOLD_GROUP
-                5 -> {
-                    text = string.substring(startIndex.plus(2), endIndex.plus(-2))
-                    val subs = clear(text)
-                    clearString += if (subs.isNullOrEmpty()) text else subs
-                    lastStartIndex = endIndex
-                }
-
-                //6 -> STRIKE_GROUP
-                6 -> {
-                    text = string.substring(startIndex.plus(2), endIndex.plus(-2))
-                    val subs = clear(text)
-                    clearString += if (subs.isNullOrEmpty()) text else subs
-                    lastStartIndex = endIndex
-                }
-
-                //7 -> RULE_GROUP
-                7 -> {
-                    clearString += " "
-                    lastStartIndex = endIndex
-                }
-
-                //8 -> INLINE CODE GROUP
-                8 -> {
-                    text = string.substring(startIndex.inc(), endIndex.dec())
-                    clearString += text
-                    lastStartIndex = endIndex
-                }
-
-                //9 -> LINK_GROUP
-                9 -> {
-                    text = string.substring(startIndex, endIndex)
-                    val (title: String, _) = "\\[(.*)]\\((.*)\\)".toRegex().find(text)!!.destructured
-                    clearString += title
-                    lastStartIndex = endIndex
-                }
-
-
-                //10 -> BLOCK CODE
-                10 -> {
-                    text = string.subSequence(startIndex.plus(3), endIndex.plus(-3)).toString()
-                    clearString += text
-                    lastStartIndex = endIndex
-                }
-
-                //11 -> ORDERED LIST
-                11 -> {
-                    val reg = "(^\\d+)".toRegex().find(string.substring(startIndex, endIndex))
-                    val order = reg!!.value
-                    text =
-                        string.subSequence(startIndex.plus(order.length + 2), endIndex).toString()
-                    val subs = clear(text)
-                    clearString += if (subs.isNullOrEmpty()) text else subs
-                    lastStartIndex = endIndex
-                }
-            }
-        }
-
-        // check if there's any more text left
-        if (lastStartIndex < string.length) {
-            clearString += string.substring(lastStartIndex, string.length)
-        }
-
-        return clearString
+        //TODO implement me
+        return null
     }
 
     /**
@@ -188,7 +62,7 @@ object MarkdownParser {
             var text: CharSequence
 
             //groups range for iterate by groups
-            val groups = 1..11
+            val groups = 1..9
             var group = -1
             for (gr in groups) {
                 if (matcher.group(gr) != null) {
@@ -291,50 +165,6 @@ object MarkdownParser {
                     text = string.subSequence(startIndex, endIndex)
                     val (title:String, link:String) = "\\[(.*)]\\((.*)\\)".toRegex().find(text)!!.destructured
                     val element = Element.Link(link, title)
-                    parents.add(element)
-                    lastStartIndex = endIndex
-                }
-                //10 -> BLOCK CODE
-                10 -> {
-                    text = string.subSequence(startIndex.plus(3), endIndex.plus(-3)).toString()
-
-                    if (text.contains(LINE_SEPARATOR)) {
-                        for ((index, line) in text.lines().withIndex()) {
-                            when (index) {
-                                text.lines().lastIndex -> parents.add(
-                                    Element.BlockCode(
-                                        Element.BlockCode.Type.END,
-                                        line
-                                    )
-                                )
-                                0 -> parents.add(
-                                    Element.BlockCode(
-                                        Element.BlockCode.Type.START,
-                                        line + LINE_SEPARATOR
-                                    )
-                                )
-                                else -> parents.add(
-                                    Element.BlockCode(
-                                        Element.BlockCode.Type.MIDDLE,
-                                        line + LINE_SEPARATOR
-                                    )
-                                )
-                            }
-                        }
-                    } else parents.add(Element.BlockCode(Element.BlockCode.Type.SINGLE, text))
-
-                    lastStartIndex = endIndex
-                }
-
-                //11 -> NUMERIC LIST
-                11 -> {
-                    val reg = "(^\\d{1,2}.)".toRegex().find(string.substring(startIndex, endIndex))
-                    val order = reg!!.value
-                    text =
-                        string.subSequence(startIndex.plus(order.length.inc()), endIndex).toString()
-
-                    val subs = findElements(text)
-                    val element = Element.OrderedListItem(order, text.toString(), subs)
                     parents.add(element)
                     lastStartIndex = endIndex
                 }
--- app/src/main/java/ru/skillbranch/skillarticles/markdown/spans/OrderedListSpan.kt
+++ app/src/main/java/ru/skillbranch/skillarticles/markdown/spans/OrderedListSpan.kt
@@ -1,11 +1,9 @@
-package ru.skillbranch.blog.markdown.spans
+package ru.skillbranch.skillarticles.markdown.spans
 
 import android.graphics.Canvas
-import android.graphics.Color
 import android.graphics.Paint
 import android.text.Layout
 import android.text.style.LeadingMarginSpan
-import android.util.Log
 import androidx.annotation.ColorInt
 import androidx.annotation.Px
 import androidx.annotation.VisibleForTesting
@@ -16,14 +14,12 @@ class OrderedListSpan(
     private val gapWidth: Float,
     private val order: String,
     @ColorInt
-    private val color: Int
+    private val orderColor: Int
 ) : LeadingMarginSpan {
     @VisibleForTesting(otherwise = VisibleForTesting.PRIVATE)
-    var orderWidth = 0f
 
     override fun getLeadingMargin(first: Boolean): Int {
-        //TODO implement me()
-        return 0
+        return (order.length.inc()*gapWidth).toInt()
     }
 
     override fun drawLeadingMargin(
@@ -31,6 +27,20 @@ class OrderedListSpan(
         lineTop: Int, lineBaseline: Int, lineBottom: Int, text: CharSequence?, lineStart: Int,
         lineEnd: Int, isFirstLine: Boolean, layout: Layout?
     ) {
-        //TODO implement me()
+        if (isFirstLine) {
+            paint.withCustomColor {
+                canvas.drawText(order,currentMarginLocation + gapWidth,lineBaseline.toFloat(),paint)
+            }
+        }
+    }
+
+    private inline fun Paint.withCustomColor(block: () -> Unit) {
+        val oldColor = color
+
+        color = orderColor
+
+        block()
+
+        color = oldColor
     }
 }
